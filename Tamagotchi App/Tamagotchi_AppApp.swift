2261a7a33124b422c3d9abca8a6264c612692c7e
Jasper Hersov <Hersov.J@etoncollege.org.uk>
2021-01-22 11:59:20 +0000

    Initial Commit

---
+++ Tamagotchi App/Tamagotchi_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tamagotchi_AppApp.swift
+//  Tamagotchi App
+//
+//  Created by Jasper Hersov on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tamagotchi_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
